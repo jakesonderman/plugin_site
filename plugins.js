6e9ad43f2f346c0119a0cc7731f1075c19c6c031
jakesondy <87723705+jakesonderman@users.noreply.github.com>
2025-04-26 15:10:44 -0400

    deploying clarity prism again again

--- plugins.js
+++ plugins.js
@@ -11,11 +11,10 @@ const plugins = [
     },
     {
         name: "Clarity Prism",
-        version: "0.1",
-        description: "multiband detail and transient destroyer.",
+        version: "1.0",
+        description: "Multiband detail and transient destroyer plugin. Includes compression, limiting, and saturation controls for achieving perfect clarity in your mixes.",
         // You can use either image or media property
-        // image: "img/1176BGND.png",
-        media: "mp4/CLARITY_PRISM_ADD.mp4", // Can be a .png, .jpg or .mov file
+        media: "mp4/CLARITY_PRISM_ADD.mp4", // Can be a .png, .jpg, .mp4, or .mov file
         downloadUrl: "downloads/prism-signed.pkg"
     }
     // Add more plugins here
